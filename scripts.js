42b194489aee6e8e1982841f02e95119b275b7fa
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2022-12-07 15:11:43 +0100

    Assign numbers 1-3 rock paper or scissors

--- scripts.js
+++ scripts.js
@@ -1,5 +1,18 @@
 
+
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() *3) +1;
     console.log(randomNumber)
+
+    if (randomNumber == 1) {
+        let ComputerChoice = "Rock"
+        console.log(ComputerChoice);
+    } else if (randomNumber == 2) {
+        let ComputerChoice = "Paper"
+        console.log(ComputerChoice);
+    } else {
+        let ComputerChoice = "Scissors"
+        console.log(ComputerChoice);
+    }
 }
+
