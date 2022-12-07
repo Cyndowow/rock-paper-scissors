7c6155a77cb81a0498a9a245a37bbccd13bc4cd7
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2022-12-07 15:15:27 +0100

    Rename ComputerChoice to computerSelection

--- scripts.js
+++ scripts.js
@@ -1,18 +1,17 @@
-
-
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() *3) +1;
     console.log(randomNumber)
 
     if (randomNumber == 1) {
-        let ComputerChoice = "Rock"
-        console.log(ComputerChoice);
+        let computerSelection = "Rock"
+        console.log(computerSelection);
     } else if (randomNumber == 2) {
-        let ComputerChoice = "Paper"
-        console.log(ComputerChoice);
+        let computerSelection = "Paper"
+        console.log(computerSelection);
     } else {
-        let ComputerChoice = "Scissors"
-        console.log(ComputerChoice);
+        let computerSelection = "Scissors"
+        console.log(computerSelection);
     }
 }
 
+function 
