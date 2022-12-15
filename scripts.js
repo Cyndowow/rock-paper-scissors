584f2fb218211d55188b8c788207e972fb02809a
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2022-12-15 17:01:07 +0100

    Remove most Debug-logs and add first UI

--- scripts.js
+++ scripts.js
@@ -6,81 +6,72 @@ let roundWinner = '';
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() *3) +1;
-    //console.log(randomNumber)
+    
 
     if (randomNumber == 1) {
         computerSelection = "rock"
-        //console.log(computerSelection);
+        
     } else if (randomNumber == 2) {
         computerSelection = "paper"
-        //console.log(computerSelection);
+        
     } else {
         computerSelection = "scissors"
-      //  console.log(computerSelection);
+      
     }
     return computerSelection;
 }
-
-function getPlayerChoice() {
-    let playerSelection = prompt("Rock, Paper or Scissors?");
-    //console.log(playerSelection);
-    return playerSelection;
-}
-
 
 
 function playRound(playerSelection, computerSelection) {
 
     computerSelection = getComputerChoice().toLowerCase();
     console.log(computerSelection);
-    playerSelection = getPlayerChoice().toLowerCase();
-    //console.log(playerSelection);
+    //playerSelection = "";
+    
 
     if (computerSelection == playerSelection) {
-        return "It's a tie!";
+        console.log("It's a tie!")
+        //return "It's a tie!";
         roundWinner = 'tie';
-        //console.log("It's a tie!");
+        
     } else if (
         (computerSelection == "rock" && playerSelection == "scissors" ) ||
         (computerSelection == "paper" && playerSelection == "rock") ||
         (computerSelection == "scissors" && playerSelection == "paper")) {
             roundWinner = 'computer';
-            return `The Computer won! ${computerSelection} beats ${playerSelection}`;
+            console.log(`The Computer won! ${computerSelection} beats ${playerSelection}`)
+            //return `The Computer won! ${computerSelection} beats ${playerSelection}`;
             
-            //console.log(`The Computer won! ${computerSelection} beats ${playerSelection}`);
+            
     } else if (
         (playerSelection == "rock" && computerSelection == "scissors" ) ||
         (playerSelection == "paper" && computerSelection == "rock") ||
         (playerSelection == "scissors" && computerSelection == "paper")){
             roundWinner = 'player';
-            return `You won! ${playerSelection} beats ${computerSelection}`;
+            console.log(`You won! ${playerSelection} beats ${computerSelection}`)
+            //return `You won! ${playerSelection} beats ${computerSelection}`;
             
-            //console.log(`You won! ${playerSelection} beats ${computerSelection}`);
+            
     } else {
         return "Error"
     }
     
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound(playerSelection, computerSelection);
-        if (roundWinner == 'computer') {
-            computerScore++;
-            console.log(playerScore + " : " + computerScore); 
-        } else if (roundWinner == 'player') {
-            playerScore++;
-            console.log(playerScore + " : " + computerScore); 
-        } else {
-            console.log(playerScore + " : " + computerScore); 
-        }
-        
-    } if (computerScore > playerScore) {
-            console.log('Computer won!');
-        } else if (playerScore > computerScore) {
-            console.log('Player won!');
-        } else {
-            console.log("It's a tie!");
-        }
-    return
-}
+const rockBtn = document.querySelector(".rock");
+rockBtn.addEventListener('click', () => {
+    let playerSelection = "rock";
+    playRound(playerSelection, computerSelection);
+});
+
+const papBtn = document.querySelector(".paper");
+papBtn.addEventListener('click', () => {
+    let playerSelection = "paper";
+    playRound(playerSelection, computerSelection);
+});
+
+const sciBtn = document.querySelector(".scissors");
+sciBtn.addEventListener('click', () => {
+    let playerSelection = "scissors";
+    playRound(playerSelection, computerSelection);
+});
